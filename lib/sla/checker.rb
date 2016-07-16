a920171f105ed7e682855a09ca221104d41a64d1
DannyB <dannyb@telesofia.com>
2016-07-16 16:42:08 +0000

    remove old code

--- lib/sla/checker.rb
+++ lib/sla/checker.rb
@@ -2,12 +2,11 @@ module SLA
   class Checker
     include Colsole
 
-    attr_accessor :max_depth, :checked_links #, :next_check
+    attr_accessor :max_depth, :checked_links
 
     def initialize
       @max_depth = 10
       @checked_links = []
-      # @next_check ||= []
     end
 
     def count
@@ -29,20 +28,5 @@ module SLA
         check sublink, depth+1, &block
       end
     end
-
-    # def check_url(url, depth, &_block)
-    #   page = Page.new url, depth: depth
-    #   page.validate
-
-    #   yield page if block_given? 
-    #   return if depth >= max_depth
-    #   return if checked_links.include? url
-
-    #   checked_links.push url
-
-    #   page.links.each do |link|
-    #     next_check.push link.url unless next_check.include? link.url
-    #   end
-    # end
   end
 end
