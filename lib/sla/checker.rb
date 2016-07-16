91f006dea1dc367dce9abce1e07e4eb3e3ee1ffb
DannyB <dannyb@telesofia.com>
2016-07-16 13:33:39 +0000

    attempt refactor without page object

--- lib/sla/checker.rb
+++ lib/sla/checker.rb
@@ -2,44 +2,47 @@ module SLA
   class Checker
     include Colsole
 
-    attr_accessor :checked_links, :max_depth, :next_check
+    attr_accessor :max_depth, :checked_links #, :next_check
 
     def initialize
       @max_depth = 10
       @checked_links = []
-      @next_check ||= []
+      # @next_check ||= []
     end
 
     def count
       checked_links.count
     end
 
-    def on_check(urls, depth=1, &block)
-      urls = [urls] if urls.is_a? String
+    def check(link, depth=1, &block)
+      link = Link.new link, depth: depth if link.is_a? String
+      link.validate
+      yield link if block_given?
 
-      self.next_check = []
-
-      urls.each do |url|
-        check_url url, depth, &block
-        if depth < max_depth && !next_check.empty?
-          on_check next_check, depth+1, &block
-        end
-      end
-    end
-
-    def check_url(url, depth, &_block)
-      page = Page.new url, depth: depth
-      page.validate
-
-      yield page if block_given? 
+      return unless link.valid?
       return if depth >= max_depth
-      return if checked_links.include? url
+      return if checked_links.include? link.url
 
-      checked_links.push url
+      checked_links.push link.url
 
-      page.links.each do |link|
-        next_check.push link.url unless next_check.include? link.url
+      link.sublinks.each do |sublink|
+        check sublink, depth+1, &block
       end
     end
+
+    # def check_url(url, depth, &_block)
+    #   page = Page.new url, depth: depth
+    #   page.validate
+
+    #   yield page if block_given? 
+    #   return if depth >= max_depth
+    #   return if checked_links.include? url
+
+    #   checked_links.push url
+
+    #   page.links.each do |link|
+    #     next_check.push link.url unless next_check.include? link.url
+    #   end
+    # end
   end
 end
--- lib/sla/link.rb
+++ lib/sla/link.rb
@@ -1,17 +1,66 @@
 module SLA
   class Link
-    attr_accessor :text, :href
+    attr_accessor :text, :href, :status, :base_uri, :depth
     attr_reader :parent
 
     def initialize(href, opts={})
-      @href = href
-      @text = opts[:text]
+      @href       = href
+      @base_uri   = href
+      @text       = opts[:text]
+      @depth      = opts[:depth] || 1
       self.parent = opts[:parent] || @href
+    end
+
+    def valid?
+      validate
+      status == '200'
+    end
+
+    def validate
+      content
+    end
+
+    def content
+      @content ||= content!
+    end
+
+    def content!
+      response = Cache.get url
+      self.status = response.error ? '404' : '200'
+      self.base_uri = response.base_uri
+      response.content
+    end
+
+    def identity
+      full_uri.request_uri
+    end
+
+    def url
+      full_uri.to_s
+    end
+
+    def doc
+      @doc ||= Nokogiri::HTML content
+    end
+
+    def sublinks
+      @sublinks ||= sublinks!
+    end
+
+    def sublinks!
+      anchors = doc.css('a')
+      result = []
+      anchors.each do |a|
+        link = Link.new a['href'], text: a.text, parent: base_uri, depth: depth+1
+        result.push link if link.interesting?
+      end
+      result
     end
 
     def uri
       @uri ||= URI.parse href
     end
+
 
     def parent=(url)
       @parent = url.is_a?(String) ? URI.parse(url) : url
