29138120ee2149c2df1a8505714e0f45e75ad971
DannyB <dannyb@telesofia.com>
2016-07-17 17:32:15 +0000

    add ability to specify hours and days in cache life

--- spec/sla/cache_spec.rb
+++ spec/sla/cache_spec.rb
@@ -9,4 +9,31 @@ describe Cache do
     end
   end
 
+  describe '::life_to_seconds' do
+    it "handles plain numbers" do
+      subject = described_class.life_to_seconds '11'
+      expect(subject).to eq 11
+    end
+
+    it "handles 11s as seconds" do
+      subject = described_class.life_to_seconds '11s'
+      expect(subject).to eq 11
+    end
+
+    it "handles 11m as minutes" do
+      subject = described_class.life_to_seconds '11m'
+      expect(subject).to eq 11 * 60
+    end
+
+    it "handles 11h as hours" do
+      subject = described_class.life_to_seconds '11h'
+      expect(subject).to eq 11 * 60 * 60
+    end
+
+    it "handles 11d as days" do
+      subject = described_class.life_to_seconds '11d'
+      expect(subject).to eq 11 * 60 * 60 * 24
+    end
+  end
+
 end
