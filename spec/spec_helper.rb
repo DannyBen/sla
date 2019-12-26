d7a2524a80d71546cafaf46bd9b47697ee6f287a
Danny Ben Shitrit <db@dannyben.com>
2019-12-26 10:13:42 +0000

    attempt fix travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ Bundler.require :default, :development
 include SLA
 
 # Enable Colsole colors in testing mode
-ENV['TTY'] = 'on'
+ENV['TTY'] = 'off'
 
 # When testing, we want clean cache
 WebCache.flush
